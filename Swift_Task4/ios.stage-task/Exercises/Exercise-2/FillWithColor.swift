fdd200b972a14be3a103017c387c9de027cd845d
Artyom Gurbovich <artyom.gurbovich@gmail.com>
2021-06-07 23:07:40 +0300

    feat: implement exercise 2

--- Swift_Task4/ios.stage-task/Exercises/Exercise-2/FillWithColor.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-2/FillWithColor.swift
@@ -1,8 +1,28 @@
 import Foundation
 
 final class FillWithColor {
-    
     func fillWithColor(_ image: [[Int]], _ row: Int, _ column: Int, _ newColor: Int) -> [[Int]] {
-        [[]]
+        guard image.count > 0 && image[0].count > 0 && row < image.count && column < image[0].count else { return image }
+        var result = image
+        let targetColor = result[row][column]
+        fillWithColorMove(&result, newColor, targetColor, row, column)
+        return result
+    }
+        
+    func fillWithColorMove(_ result: inout [[Int]], _ newColor: Int, _ targetColor: Int , _ row: Int, _ column: Int) {
+        guard result[row][column] == targetColor && result[row][column] != newColor else { return }
+        result[row][column] = newColor
+        if row > 0 {
+            fillWithColorMove(&result, newColor, targetColor, row - 1, column)
+        }
+        if row + 1 < result.count {
+            fillWithColorMove(&result, newColor, targetColor, row + 1, column)
+        }
+        if column > 0 {
+            fillWithColorMove(&result, newColor, targetColor, row, column - 1)
+        }
+        if column + 1 < result[0].count {
+            fillWithColorMove(&result, newColor, targetColor, row, column + 1)
+        }
     }
 }
